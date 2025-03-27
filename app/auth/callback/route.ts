adc54777a4f7739ab52d6c272ba3882c28dfdfcc
latttchc <iam.yuk2882@gmail.com>
2025-03-27 16:38:07 +0900

    認証コンポーネントと機能の追加

---
+++ app/auth/callback/route.ts
@@ -0,0 +1,21 @@
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+
+export const GET = async (req: NextRequest) => {
+    const cookieStore = cookies();
+    const supabase = createRouteHandlerClient({ cookies: () => cookieStore })
+
+    const { searchParams } = new URL(req.url);
+
+    //　codeがあった場合はセッションに含める
+    const code = searchParams.get('code');
+
+    // codeがあった場合はセッションに含める
+    if (code) {
+        await supabase.auth.exchangeCodeForSession(code);
+    }
+
+    // リスト一覧へ偏移
+    return NextResponse.redirect(new URL('/todo-list', req.url));
+}
--- app/components/AuthForm.tsx
+++ app/components/AuthForm.tsx
@@ -1,1 +1,28 @@
 'use client'
+
+import { Card, Typography } from "@mui/material"
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"
+import { Auth } from "@supabase/auth-ui-react"
+import { ThemeSupa } from "@supabase/auth-ui-shared"
+
+// 認証コンポート
+const AuthForm = () => {
+    const supabase = createClientComponentClient();
+    return (
+        <Card sx={{ maxWidth: 600, p: 5 }}>
+            <Typography gutterBottom variant="h5" component="div">
+                認証フォーム
+            </Typography>
+            <Auth
+                supabaseClient={supabase}
+                view="magic_link"
+                showLinks={false}
+                providers={[]}
+                redirectTo="http://localhost:3000/auth/callback"
+                appearance={{ theme: ThemeSupa }}
+            />
+        </Card>
+    )
+}
+
+export default AuthForm;
