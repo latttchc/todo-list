49dd2412c6d23e024ea571f36ab904efafe1b8b5
latttchc <iam.yuk2882@gmail.com>
2025-03-27 16:46:10 +0900

    ログアウト機能の実装

---
+++ app/auth/signout/route.ts
@@ -0,0 +1,17 @@
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+
+export const POST = async (req: NextRequest) => {
+    const cookiesStore = cookies();
+    const supabase = createRouteHandlerClient({ cookies: () => cookiesStore });
+
+    //　supabaseからセッションを取得
+    const { data: { session } } = await supabase.auth.getSession();
+
+    if (session) {
+        await supabase.auth.signOut();
+    }
+
+    return NextResponse.redirect(new URL('/', req.url));
+}
