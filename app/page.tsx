ecd1ad2cee90e92149f0030c6f3873eb6ecb9d97
latttchc <iam.yuk2882@gmail.com>
2025-03-27 17:47:14 +0900

    認証画面のレイアウトとページの作成

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,10 @@ import AuthForm from "./components/AuthForm";
 
 const Home = () => {
   return (
-    <Box display="flex" justifyContent="center">
+    <Box display="flex" justifyContent="center" sx={{ mt: 5 }}>
       <Box sx={{ my: 2 }}>
-        <Typography variant="h2">Welcome! TodoLIST!!</Typography>
-        <Alert sx={{ my: 2 }}>
+        <Typography variant="h2">Welcome to My TODO!!</Typography>
+        <Alert sx={{ my: 4 }}>
           メールアドレスを登録して利用できます。
         </Alert>
         <Box display="flex" justifyContent="center">
