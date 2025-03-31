2953ae1f1f9f35a0f7b55cc1da11d65245db82f0
latttchc <iam.yuk2882@gmail.com>
2025-03-31 22:30:03 +0900

    next exportは使えないので修正
    output: "export" の設定が推奨。これにより、静的サイトとしてエクスポートするために next.config.js で明示的に output: "export" を設定する必要がある。

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,7 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
-  basePath: process.env.GITHUB_PAGES ? "/todo-list" : "",
-
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "@": path.resolve(__dirname, "./app"),
-    };
-    return config;
-  },
+  output: "export",
 };
 
 export default nextConfig;
